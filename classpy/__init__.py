b3b61c95f86918954c1d69dfb8276f53d2889bab
connor-kress <con2coool@gmail.com>
2023-10-27 15:47:52 -0400

    Created file structure for testing.

---
+++ classpy/__init__.py
@@ -0,0 +1,5 @@
+from .test import Test
+
+__all__ = [
+    Test
+]
---
+++ classpy/main.py
@@ -0,0 +1,11 @@
+import requests
+import bs4
+from classpy import Test
+
+
+def main() -> None:
+    print(Test.works)
+
+
+if __name__ == '__main__':
+    main()
