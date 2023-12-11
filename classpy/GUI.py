b568abe14b6858b0ed36deb250a35744800e3b11
Shenyu Zhou <zhoushenyu@ufl.edu>
2023-12-10 22:32:32 -0500

    course_query() GUI updated with ratemyprof scores disp

--- classpy/GUI.py
+++ classpy/GUI.py
@@ -2,9 +2,14 @@ import tkinter as tk
 from tkinter import ttk
 import asyncio
 import threading
+import pandas as pd
+import csv
 
 from classpy import *
 
+# read the course score csv to be displayed
+course_score_df = pd.read_csv("./data/raw_data/course_with_score.csv")
+class_scores = course_score_df.set_index('Class Digit')['Score'].to_dict()
 
 class CourseQueryGUI:
     def setup(self) -> None:
@@ -41,7 +46,7 @@ class CourseQueryGUI:
         search_button = tk.Button(root, text="Search", command=on_search_click)
 
         # treeview columns (without Classes attributes)
-        columns = ('course number', 'title', 'description', 'credits',
+        columns = ('course code', 'title', 'description', 'credits',
                 'department', 'requirements', 'fees', 'EEP_eligible', 'gen_ed')
 
         # display the attributes
@@ -51,17 +56,22 @@ class CourseQueryGUI:
             results_tree.heading(col, text=col.capitalize())
             results_tree.column(col, width=150, anchor='center')
 
-        # scrollbar and display sizes
+        # scrollbar
         h_scrollbar = ttk.Scrollbar(root, orient='horizontal', command=results_tree.xview)
-        results_tree.configure(xscroll=h_scrollbar.set)
-        h_scrollbar.grid(row=10, column=0, columnspan=3, sticky='ew')
-
-        scrollbar = ttk.Scrollbar(root, orient='vertical', command=results_tree.yview)
-        results_tree.configure(yscroll=scrollbar.set)
-        scrollbar.grid(row=9, column=3, sticky='ns')
+        results_tree.configure(xscrollcommand=h_scrollbar.set)
+        v_scrollbar = ttk.Scrollbar(root, orient='vertical', command=results_tree.yview)
+        results_tree.configure(yscrollcommand=v_scrollbar.set)
 
         # set the TreeView layout
         results_tree.grid(row=9, column=0, columnspan=2, sticky='nsew')
+
+        # place scrollbars in the grid
+        h_scrollbar.grid(row=10, column=0, columnspan=3, sticky='ew')
+        v_scrollbar.grid(row=9, column=3, sticky='ns')
+
+        # configure the grid rows and columns to adjust with window resizing
+        root.grid_rowconfigure(9, weight=1)
+        root.grid_columnconfigure(0, weight=1)
 
         # TreeView bind selection
         results_tree.bind('<<TreeviewSelect>>', show_course_details)
@@ -131,20 +141,20 @@ def add_course_to_treeview(tree: ttk.Treeview, course: Course):
     class_details = ''
     for class_obj in course.available_classes:
         class_detail = (
-            f'Number: {class_obj.number}, '
-            f'Instructors: {', '.join(class_obj.instructors) if
-                            class_obj.instructors else 'none'}, '
-            f'Online: {'Yes' if class_obj.is_online else 'No'}, '
+            f'Instructors: {", ".join(class_obj.instructors) if class_obj.instructors else "none"}, ' \
+            f'Online: {"Yes" if class_obj.is_online else "No"}, '
         )
+        class_details += f"{class_obj.number}, "
         if class_obj.final_exam_time:
             class_detail += (
-                f'Exam Time: {class_obj.final_exam_time[0].strftime('%Y-%m-%d %H:%M')} '
-                f'to {class_obj.final_exam_time[1].strftime('%Y-%m-%d %H:%M')}, '
+                f"Exam Time: {class_obj.final_exam_time[0].strftime('%Y-%m-%d %H:%M')} "\
+                f"to {class_obj.final_exam_time[1].strftime('%Y-%m-%d %H:%M')}, "
+
             )
         class_detail += (
-            f'Dates: {class_obj.class_dates[0].strftime('%Y-%m-%d')} to '
-                f'{class_obj.class_dates[1].strftime('%Y-%m-%d')}, '
-            f'Classrooms: {', '.join(room.code for room in class_obj.classrooms)}'
+            f"Dates: {class_obj.class_dates[0].strftime('%Y-%m-%d')} to " \
+            f"{class_obj.class_dates[1].strftime('%Y-%m-%d')}, "
+            f'Classrooms: {", ".join(room.code for room in class_obj.classrooms)}'
         )
         class_details += class_detail + '\n'
     class_details.removesuffix('\n')
@@ -164,7 +174,6 @@ def add_course_to_treeview(tree: ttk.Treeview, course: Course):
         class_details,
     ))
 
-
 def on_search_click():
     """Runs a course query on a new thread."""
     thread = threading.Thread(target=threaded_course_query)
@@ -172,15 +181,58 @@ def on_search_click():
 
 
 def show_course_details(_event):
-    """Shows more in depth details for a course in a pop-up."""
+    """Shows more in-depth details for a course in a pop-up."""
     selected_item = __gui.results_tree.selection()[0]
     course = __gui.results_tree.item(selected_item, 'values')
-    detailed_info = f'Course Number: {course[0]}\nTitle: '\
-                    f'{course[1]}\nDescription: {course[2]}\n'\
-                    f'Requirement: {course[5]}\nSessions: {course[9]}'
-    detail_window = tk.Toplevel(__gui.root)
-    detail_window.title("Course Details")
-    tk.Label(detail_window, text=detailed_info).pack()
+    sessions = course[9].split('\n')
+
+    # create a new window for session selection
+    session_window = tk.Toplevel(__gui.root)
+    session_window.title("Select a Session")
+
+    # add a Listbox to display all sessions
+    lb = tk.Listbox(session_window, width=80)
+    for i, session in enumerate(sessions):
+        if session:
+            # display only number, instructor, and score
+            parts = session.split(",")
+            session_code = parts[0].strip()
+            class_code = f"Class #{session_code}" # for consistency with the csv vals
+            instructor = next((part.strip() for part in parts if "Instructors:" in part), 'Unknown Instructor')
+            score = class_scores.get(class_code, pd.NA)
+            sessions[i] += f"\nScore: {score}" # add specific score to each session for detailed display
+            display_text = f"Session: {session_code}, {instructor}, Score: {score}"
+            lb.insert(tk.END, display_text)
+    lb.pack()
+
+    # function to handle session selection and display details
+    def on_session_select(event):
+        selected_index = lb.curselection()[0]
+        selected_session = sessions[selected_index]
+
+        detailed_info = (f'Course Code: {course[0]}\n'
+                         f'Title: {course[1]}\n'
+                         f'Description: {course[2]}\n'
+                         f'Requirement: {course[5]}\n'
+                         f'Session: {selected_session}\n')
+
+        # display course details independently
+        detail_window = tk.Toplevel(__gui.root)
+        detail_window.title("Course Details")
+
+        # create a Text widget with a Scrollbar
+        text = tk.Text(detail_window, wrap='word')
+        scrollbar = tk.Scrollbar(detail_window, command=text.yview)
+        text.configure(yscrollcommand=scrollbar.set)
+
+        text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        text.insert(tk.END, detailed_info)
+        text.config(state=tk.DISABLED)  # Make the text widget read-only
+
+    # bind the selection event to the Listbox
+    lb.bind("<<ListboxSelect>>", on_session_select)
 
 
 def main() -> None:
