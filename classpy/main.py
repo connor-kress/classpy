7b8bd33f292635cddcf26a1c9e919a27a06f0a84
connor-kress <con2coool@gmail.com>
2023-12-11 23:22:31 -0500

    Updated project report PDF.

--- classpy/main.py
+++ classpy/main.py
@@ -1,24 +1,16 @@
 import asyncio
-import pprint
+from pprint import pprint
 from playwright.async_api import async_playwright
 
 from classpy import *
 
 
 async def main() -> None:
-    print(await ScheduleBuider.build())
-    # print = pprint.PrettyPrinter(depth=5).pprint
-    # async with async_playwright() as playwright:
-    #     browser = await playwright.chromium.launch(headless=True)
-    #     ctx = await browser.new_context()
-        
-    #     builder = ScheduleBuider()
-    #     print(await builder.build_raw(ctx))
-    #     # course = (await course_query(title='Programming fundamentals 2'))[0]
-    #     # print(course)
+    # course = (await course_query(title='Programming fundamentals 2'))[0]
+    # pprint(course)
 
-    #     await ctx.close()
-    #     await browser.close()
+    schedule = await ScheduleBuider.build()
+    print(schedule)
 
 
 if __name__ == '__main__':
